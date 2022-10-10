e3a1980b253de3bfbf9a8f007e670d0fe16aa4dc
joaovitormsilva <joaovitormsilva1@gmail.com>
2022-10-10 17:49:18 -0300

    Atualizando o pc

--- .FlutterProjects/ProjetoCalculadora/aula01/versao01/lib/main.dart
+++ .FlutterProjects/ProjetoCalculadora/aula01/versao01/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp);
+  runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
@@ -12,8 +12,134 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  String valor = '0';
+
+  void mudarnumero(String valor, int digito) {
+    String aux = digito.toString();
+    setState(() {
+      valor = valor + aux;
+      print(valor);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return;
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center( child:Text("Calculadora"),
+        ),),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+        
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                height: 300,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: const [
+              Text(
+                'AC',
+                style: TextStyle(fontSize: 40),
+              ),
+              Text(
+                'X',
+                style: TextStyle(fontSize: 40),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: const [
+              Text(
+                '7',
+                style: TextStyle(fontSize: 40),
+              ),
+              Text(
+                '8',
+                style: TextStyle(fontSize: 40),
+              ),
+              Text(
+                '9',
+                style: TextStyle(fontSize: 40),
+              ),
+              Text(
+                '/',
+                style: TextStyle(fontSize: 40),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: const [
+              Text(
+                '4',
+                style: TextStyle(fontSize: 40),
+              ),
+              Text(
+                '5',
+                style: TextStyle(fontSize: 40),
+              ),
+              Text(
+                '6',
+                style: TextStyle(fontSize: 40),
+              ),
+              Text(
+                '*',
+                style: TextStyle(fontSize: 40),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: const [
+              Text(
+                '1',
+                style: TextStyle(fontSize: 40),
+              ),
+              Text(
+                '2',
+                style: TextStyle(fontSize: 40),
+              ),
+              Text(
+                '3',
+                style: TextStyle(fontSize: 40),
+              ),
+              Text(
+                '-',
+                style: TextStyle(fontSize: 40),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: const [
+              Text(
+                '0',
+                style: TextStyle(fontSize: 40),
+              ),
+              Text(
+                ',',
+                style: TextStyle(fontSize: 40),
+              ),
+              Text(
+                '=',
+                style: TextStyle(fontSize: 40),
+              ),
+              Text(
+                '+',
+                style: TextStyle(fontSize: 40),
+              ),
+            ],
+          ),
+        ]),
+      ),
+    );
   }
 }
