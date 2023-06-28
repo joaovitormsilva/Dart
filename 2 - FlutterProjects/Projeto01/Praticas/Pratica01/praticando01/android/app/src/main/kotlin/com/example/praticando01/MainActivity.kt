be42f173595a3d036c374495f98f129ea3ced83f
joaovitormsilva <joaovitormsilva1@gmail.com>
2023-06-28 17:58:19 -0300

    Arrumado

--- 2 - FlutterProjects/Projeto01/Praticas/Pratica01/praticando01/android/app/src/main/kotlin/com/example/praticando01/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.praticando01
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- 4 - Experimentos/control_light/android/app/src/main/kotlin/com/example/control_light/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.control_light
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
